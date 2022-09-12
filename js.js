6e2b9df61123ef5dd8430739f78e7e44c9a30709
id39 <100359834+idme520@users.noreply.github.com>
2022-09-12 14:45:53 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+function kuang() {
+  let kuang = document.getElementById("biankuang");
+  let box = document.getElementById("abox");
+  if (kuang.onclick.style.display === "block") {
+    box.style.display = "none";
+  } else {
+    box.display.style = "none";
+  }
+} //这是废弃的,js还是有点不懂
+function caidanlan() {
+  //菜单
+  let as = document.getElementById("cai3");
+  if (cai3.style.display === "none") {
+    cai3.style.display = "block";
+  } else {
+    cai3.style.display = "none";
+  }
+}
+function shezhilan() {
+  //未命名
+  let au = document.getElementById("cai4");
+  let ae = document.getElementById("shezhilan");
+  if (cai4.style.display === "none") {
+    cai4.style.display = "block";
+  } else {
+    cai4.style.display = "none";
+  }
+}
